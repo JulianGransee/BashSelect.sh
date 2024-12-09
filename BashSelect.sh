3137620ebaa0ebb5bcde2c8b2e64b178c97db9ef
Twe3x <holl.simon20021@gmail.com>
2024-12-09 12:51:06 +0100

    replaced tput
    Replaced tput commands with ANSI escape sequences

--- BashSelect.sh
+++ BashSelect.sh
@@ -17,52 +17,49 @@
 #!/bin/bash
 
 function bashSelect() {
-  function printOptions () #printing the different options
-  {
-  it=$( echo $1)
-  for i in ${!OPTIONS[*]}; do
-    if [[ "$i" == "$it" ]]; then
-      tput rev
-      printf '%4d ) %s\n' $i "${OPTIONS[$i]}"
-      tput sgr0
-    else
-      printf '%4d ) %s\n' $i "${OPTIONS[$i]}"
-    fi
-  done
+  function printOptions() { # printing the different options
+    it=$1
+    for i in "${!OPTIONS[@]}"; do
+      if [[ "$i" -eq "$it" ]]; then
+        echo -e "\033[7m  $i) ${OPTIONS[$i]} \033[0m"
+      else
+        echo "  $i) ${OPTIONS[$i]}"
+      fi
+    done
   }
 
-  tput civis
+  trap 'echo -ne "\033[?25h"; exit' SIGINT SIGTERM
+  echo -ne "\033[?25l"
   it=0
 
   printOptions $it
 
-  while true; do #loop through array to capture every key press until enter is pressed
-    #capute key input
+  while true; do # loop through array to capture every key press until enter is pressed
+    # capture key input
     read -rsn1 key
-    escaped_char=$( printf "\u1b" )
-    if [[ $key == $escaped_char ]]; then
-        read -rsn2 key
+    if [[ $key == $'\x1b' ]]; then
+      read -rsn2 key
     fi
-    tput cuu ${#OPTIONS[@]} && tput ed
-    tput sc
 
-    #handle key input
+    echo -ne "\033[${#OPTIONS[@]}A\033[J"
+
+    # handle key input
     case $key in
-        '[A' | '[C' )
-            it=$(($it-1));;
-        '[D' | '[B')
-            it=$(($it+1));;
-        '' )
-            tput cnorm
-            return $it && exit;;
+      '[A' | '[C') ((it--));; # up or right arrow
+      '[B' | '[D') ((it++));; # down or left arrow
+      '')
+        echo -ne "\033[?25h"
+        return "$it"
+        ;;
     esac
-    #manage that you can't select something out of range
+
+    # manage that you can't select something out of range
     min_len=0
-    farr_len=$(( ${#OPTIONS[@]}-1))
+    max_len=$(( ${#OPTIONS[@]} - 1 ))
     if [[ "$it" -lt "$min_len" ]]; then
-      it=$(( ${#OPTIONS[@]}-1 ))
-    elif [[ "$it" -gt "$farr_len"  ]]; then
-      it=0
+      it=$max_len
+    elif [[ "$it" -gt "$max_len" ]]; then
+      it=$min_len
     fi
 
     printOptions $it
